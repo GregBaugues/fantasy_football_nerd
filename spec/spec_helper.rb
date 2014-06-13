402f3d9e701f60584272b46390a38e0155e17633
Greg Baugues <greg@baugues.com>
2014-06-13 18:50:14 -0500

    "treat_symbols_as_metadata_keys_with_true_values= is deprecated..."

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,6 @@
 require 'rubygems'
 require 'vcr'
 
-RSpec.configure do |config|
-    config.treat_symbols_as_metadata_keys_with_true_values = true
-end
-
 VCR.configure do |c|
   c.cassette_library_dir = "spec/vcr/cassettes"
   c.hook_into :webmock
