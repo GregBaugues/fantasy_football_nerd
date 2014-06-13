cf0a84a651dc160e8efc447d496f678d5a1cab54
Greg Baugues <greg@baugues.com>
2014-06-13 18:50:36 -0500

    update spec to only allow json

--- spec/url_spec.rb
+++ spec/url_spec.rb
@@ -8,8 +8,4 @@ describe 'Urls' do
     expect(service_url('nfl-teams', '123')).to eq "http://www.fantasyfootballnerd.com/service/nfl-teams/json/123"
   end
 
-  it 'should allow an xml format' do
-    expect(service_url('nfl-teams', '123', :xml)).to eq "http://www.fantasyfootballnerd.com/service/nfl-teams/xml/123"
-  end
-
 end
