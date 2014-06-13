1821575801343c6810387d31fe9857c23e7aa9ba
Greg Baugues <greg@baugues.com>
2014-06-13 18:30:01 -0500

    Add the beginnings of requests.rb

---
+++ lib/requests.rb
@@ -0,0 +1,12 @@
+require './lib/urls.rb'
+require 'open-uri'
+
+module Requests
+  include Urls
+
+  def request_service(service, api_key)
+    url = service_url(service, api_key)
+    JSON.parse(open(url))
+  end
+
+end
