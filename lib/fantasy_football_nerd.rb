f0a4ea8c888c0f00bc4dde6678965e01eae264c1
Greg Baugues <greg@baugues.com>
2014-06-13 18:42:44 -0500

    Add rspec to gemfile

--- lib/fantasy_football_nerd.rb
+++ lib/fantasy_football_nerd.rb
@@ -2,5 +2,10 @@ require 'json'
 require './lib/requests.rb'
 
 class FFNerd
+  @@api_key
+
+  def self.api_key=(value)
+    @@api_key = value
+  end
 
 end
--- spec/fantasy_football_nerd_spec.rb
+++ spec/fantasy_football_nerd_spec.rb
@@ -3,6 +3,9 @@ require_relative '../lib/fantasy_football_nerd.rb'
 
 describe 'Fantasy Football Nerd Gem' do
 
-
+  it 'should set an api key' do
+    FFNerd.api_key = "1234"
+    expect(FFNerd.api_key).to eq "1234"
+  end
 
 end
