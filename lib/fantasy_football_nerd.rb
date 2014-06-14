3fc9b9e33881bbd2ec7c01d4e614956bda7067f9
Greg Baugues <greg@baugues.com>
2014-06-13 23:35:46 -0500

    Refactor to use FFNerd.ostruct_request

--- lib/fantasy_football_nerd.rb
+++ lib/fantasy_football_nerd.rb
@@ -1,38 +1,43 @@
 require 'json'
 require 'ostruct'
-require './lib/requests.rb'
+require './lib/request.rb'
 
 class FFNerd
-  extend Requests
+  extend Request
 
   def self.api_key
     raise 'API key not set' unless ENV['FFNERD_API_KEY']
     ENV['FFNERD_API_KEY']
   end
 
+  def self.ostruct_request(service_name, json_key)
+    response = request_service(service_name, api_key)
+    response[json_key].collect { |i| OpenStruct.new(i) }
+  end
+
   def self.teams
-    response = request_service('nfl-teams', api_key)
-    response['NFLTeams'].collect { |t| OpenStruct.new(t) }
+    ostruct_request('nfl-teams', 'NFLTeams')
   end
 
   def self.schedule
-    response = request_service('schedule', api_key)
-    response['Schedule'].collect { |g| OpenStruct.new(g) }
+    ostruct_request('schedule', 'Schedule')
+  end
+
+  def self.players
+    ostruct_request('players', 'Players')
+  end
+
+  def self.auction_values
+    ostruct_request('auction', 'AuctionValues')
+  end
+
+  def self.ppr_draft_projections
+    ostruct_request('draft-projections', 'DraftRankings')
   end
 
   def self.current_week
     response = request_service('schedule', api_key)
     response['currentWeek']
-  end
-
-  def self.players
-    response = request_service('players', api_key)
-    response['Players'].collect { |p| OpenStruct.new(p) }
-  end
-
-  def self.auction_values
-    response = request_service('auction', api_key)
-    response['AuctionValues'].collect { |av| OpenStruct.new(av) }
   end
 
 end
