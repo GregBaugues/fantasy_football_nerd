046ee34d1c218408a480b1652b5bdd2d6f985598
Greg Baugues <greg@baugues.com>
2014-07-11 12:41:31 -0500

    Add player tests

--- spec/fantasy_football_nerd_spec.rb
+++ spec/fantasy_football_nerd_spec.rb
@@ -10,16 +10,33 @@ describe 'Fantasy Football Nerd Gem', vcr: true do
   it 'should retrieve teams' do
     team = FFNerd.teams.first
     expect(team.code).to eq "ARI"
-    expect(team.fullName).to eq "Arizona Cardinals"
-    expect(team.shortName).to eq "Arizona"
+    expect(team.full_name).to eq "Arizona Cardinals"
+    expect(team.short_name).to eq "Arizona"
   end
 
   it 'should retrieve the schedule' do
-    expect(FFNerd.schedule.first).to be
+    game = FFNerd.schedule.first
+    expect(game.game_id).to eq "1"
+    expect(game.game_week).to eq "1"
+    expect(game.game_date).to eq "2013-09-05"
+    expect(game.away_team).to eq "BAL"
+    expect(game.home_team).to eq "DEN"
   end
 
   it 'should retrieve players' do
-    expect(FFNerd.players.first).to be
+    player = FFNerd.players.first
+    expect(player.player_id).to eq "2"
+    expect(player.active).to eq "1"
+    expect(player.jersey).to eq "3"
+    expect(player.lname).to eq "Anderson"
+    expect(player.fname).to eq "Derek"
+    expect(player.display_name).to eq "Derek Anderson"
+    expect(player.team).to eq "CAR"
+    expect(player.position).to eq "QB"
+    expect(player.height).to eq "6-6"
+    expect(player.weight).to eq "240"
+    expect(player.dob).to eq "1983-06-15"
+    expect(player.college).to eq "Oregon State"
   end
 
   it 'should retrieve bye weeks' do
