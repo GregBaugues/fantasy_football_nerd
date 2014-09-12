b666553108a7cc520d2482f968ec43e88d6d1a40
Greg Baugues <greg@baugues.com>
2014-09-12 12:11:31 -0500

    Change default VCR recording to :new_episodes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,11 @@
 require 'rubygems'
 require 'vcr'
 
-ENV['FFNERD_API_KEY'] = 'test'
-
 VCR.configure do |c|
   c.cassette_library_dir = "spec/vcr/cassettes"
   c.hook_into :webmock
   c.filter_sensitive_data('APIKEY') { ENV['FFNERD_API_KEY'] }
+  c.default_cassette_options = { record: :new_episodes }
   c.configure_rspec_metadata!
 end
 
